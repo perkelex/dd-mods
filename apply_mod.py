f8663c83093f242e95167a5c043aee2bd99a3be2
perkelex <orlando.mitrut@gmail.com>
2023-04-13 22:37:51 +0300

    Added mod apply options: single or all

--- apply_mod.py
+++ apply_mod.py
@@ -3,18 +3,24 @@ import configparser
 import shutil
 from pathlib import Path
 
+exclusions = [".venv", ".git"]
+
 argparser = argparse.ArgumentParser(
     prog="apply",
     description="Applies mod to DarkestDungeon installation configured in mods.ini"
 )
 
-argparser.add_argument("mod_name")
+argparser.add_argument("--one", dest="one_mod_name", default=None, required=False)
+argparser.add_argument("all")
 args = argparser.parse_args()
 
 config = configparser.ConfigParser()
 config.read("mods.ini")
 
-src = Path(args.mod_name).absolute()
 dst = Path(config["GAME"]["Location"]).absolute()
 
-shutil.copytree(src, dst, dirs_exist_ok=True)
+if (args.one_mod_name):
+    shutil.copytree(Path(args.one_mod_name).absolute(), dst, dirs_exist_ok=True)
+elif(args.all):
+    for mod in [file for file in Path(".").glob('*') if file.is_dir() and file.stem not in exclusions]:
+        shutil.copytree(mod.absolute(), dst, dirs_exist_ok=True)
